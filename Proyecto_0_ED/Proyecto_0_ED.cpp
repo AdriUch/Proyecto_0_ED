0536886b89cf8f4691e1a15912a6ba799e1411bd
Carmen Hidalgo <hidalgo.paz.carmen@gmail.com>
2024-10-02 16:09:35 -0600

    Agregando el menú
    Ya se tiene un menú para algunas de las opciones. Todavía faltan agregar algunos sub menús pero ya están las bases de hacerlo

--- Proyecto_0_ED/Proyecto_0_ED.cpp
+++ Proyecto_0_ED/Proyecto_0_ED.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <time.h>
+#include <conio.h> // Para elección del menú
+
 #include "LinkedPriorityQueue.h"
 #include "MinHeap.h"
 #include "HeapPriorityQueue.h"
@@ -9,48 +11,250 @@
 #include "Tiquete.h"
 
 using std::cout;
+using std::cin;
 using std::endl;
 using std::string;
 using std::getline;
 using std::to_string;
 
+// - - - - -  - - - - - MENÚS - - - - - - - - - -
+
+
+// Función para mostrar un menú
+void mostrarMenu(int currentSelection, const string opciones[], int size) {
+	system("cls");
+
+	for (int i = 0; i < size; ++i) {
+		if (i == currentSelection) {
+			cout << " > " << opciones[i] << endl;
+		}
+		else {
+			cout << "   " << opciones[i] << endl;
+		}
+	}
+}
+
+// - - - - - MENÚS TIQUETES - - - - -
+
+//Sub-menú del principal. Genera un tiquete
+bool menuTiquetes() {
+	const int menuSize = 2;
+	string opciones[menuSize] = { "Tipo de Usuario", "Regresar" };
+	int currentSelection = 0;
+
+	while (true) {
+		mostrarMenu(currentSelection, opciones, menuSize);
+
+		int key = _getch();
+		if (key == 224) {
+			key = _getch();
+			switch (key) {
+			case 72:
+				if (currentSelection > 0) {
+					currentSelection--;
+				}
+				break;
+			case 80:
+				if (currentSelection < menuSize - 1) {
+					currentSelection++;
+				}
+				break;
+			}
+		}
+		else if (key == 13) {
+			system("cls");
+			
+			if (opciones[currentSelection] == "Tipo de Usuario") {
+				cout << "estoy aqui, wooo" << endl;
+			}
+			if (currentSelection == menuSize - 1) {
+				return false;
+			}
+			system("pause");
+		}
+	}
+}
+
+// - - - - - MENÚS ADMINISTRACIÓN - - - - -
+
+// Sub-menú de Admin. Realiza las operaciones de los usuarios.
+bool menuTipoUsuarios(PriorityQueue<Usuario>* userList) {
+	const int menuSize = 3;
+	string opciones[menuSize] = { "Agregar", "Eliminar", "Regresar" };
+	int currentSelection = 0;
+
+	while (true) {
+		mostrarMenu(currentSelection, opciones, menuSize);
+
+		int key = _getch();
+		if (key == 224) {
+			key = _getch();
+			switch (key) {
+			case 72:
+				if (currentSelection > 0) {
+					currentSelection--;
+				}
+				break;
+			case 80:
+				if (currentSelection < menuSize - 1) {
+					currentSelection++;
+				}
+				break;
+			}
+		}
+		else if (key == 13) {
+			system("cls");
+
+			if (opciones[currentSelection] == "Agregar") {
+				string userName;
+				int userPriority;
+				cout << "Ingrese el nombre de usuario que desea agregar: ";
+				getline(cin, userName);
+
+				while (true) {
+					cout << endl << "Ingrese la prioridad del tipo de usuario: ";
+					cin >> userPriority;
+
+					if (cin.fail()) {
+						cin.clear();
+						cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+						cout << "* Por favor, ingrese un numero valido *" << endl;
+					}
+					else {
+						cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+						break;
+					}
+				}
+
+				Usuario user(userPriority, userName);
+				userList->insert(user, user.getPriority());
+
+				userList->print();
+
+				cout << endl << "* Accion realizada con exito *" << endl;
+			}
+			if (opciones[currentSelection] == "Eliminar") {
+				cout << "estoy aqui, wooo" << endl;
+			}
+			if (currentSelection == menuSize - 1) {
+				return false;
+			}
+			system("pause");
+		}
+	}
+}
+
+//Sub-menú del principal. Se encarga de dar opciones para listas o colas de usuarios, áres y servicios
+bool menuAdmin(PriorityQueue<Usuario>* userList) {
+	const int menuSize = 4;
+	string opciones[menuSize] = { "Tipo de Usuario", "Areas", "Servicios disponibles", "Regresar"};
+	int currentSelection = 0;
+
+	while (true) {
+		mostrarMenu(currentSelection, opciones, menuSize);
+
+		int key = _getch();
+		if (key == 224) {
+			key = _getch();
+			switch (key) {
+			case 72:
+				if (currentSelection > 0) {
+					currentSelection--;
+				}
+				break;
+			case 80:
+				if (currentSelection < menuSize - 1) {
+					currentSelection++;
+				}
+				break;
+			}
+		}
+		else if (key == 13) {
+			system("cls");
+
+			if (opciones[currentSelection] == "Tipo de Usuario") {
+				bool tipoUsuariosMenu = menuTipoUsuarios(userList);
+				if (!tipoUsuariosMenu) {
+					continue;
+				}
+			}
+			if (opciones[currentSelection] == "Areas") {
+				cout << "estoy aqui, wooo" << endl;
+			}
+			if (opciones[currentSelection] == "Servicios disponibles") {
+				cout << "estoy aqui, wooo" << endl;
+			}
+			if (currentSelection == menuSize - 1) {
+				return false;
+			}
+			system("pause");
+		}
+	}
+}
+
+
+
+// - - - - - MENÚ PRINCIPAL - - - - -
+void menuPrincipal(PriorityQueue<Usuario>* userList) {
+	const int menuSize = 6;
+	string opciones[menuSize] = { "Estado de las colas", "Tiquetes", "Atender", "Administracion", "Estadisticas del sistema", "Exit"};
+	int currentSelection = 0;
+
+	while (true) {
+		mostrarMenu(currentSelection, opciones, menuSize);
+
+		int key = _getch();
+		if (key == 224) {
+			key = _getch();
+			switch (key) {
+			case 72:
+				if (currentSelection > 0) {
+					currentSelection--;
+				}
+				break;
+			case 80:
+				if (currentSelection < menuSize - 1) {
+					currentSelection++;
+				}
+				break;
+			}
+		}
+		else if (key == 13) {
+			system("cls");
+
+			if (opciones[currentSelection] == "Estado de las colas") {
+				cout << "estoy aqui, wooo";
+			}
+			if (opciones[currentSelection] == "Tiquetes") {
+				bool tiquetesMenu = menuTiquetes();
+				if (!tiquetesMenu) {
+					continue;
+				}
+			}
+			if (opciones[currentSelection] == "Atender") {
+				cout << "estoy aqui, wooo";
+			}
+			if (opciones[currentSelection] == "Administracion") {
+				bool adminMenu = menuAdmin(userList);
+				if (!adminMenu) {
+					continue;
+				}
+			}
+			if (opciones[currentSelection] == "Estadisticas del sistema") {
+				cout << "estoy aqui, wooo";
+			}
+			if (currentSelection == menuSize - 1) {
+				break;
+			}
+			system("pause");
+		}
+	}
+}
+
 int main() {
 	PriorityQueue<Usuario>* colaUsuarios = new HeapPriorityQueue<Usuario>();
-	PriorityQueue<Tiquete>* colaTiquetes = new HeapPriorityQueue<Tiquete>();
-	
-	Usuario user(0, "hello");
-	Usuario user2(2, "todo");
-	Usuario user3(1, "bien");
-	Usuario user4(4, "por");
-	Usuario user5(0, "aqui");
-	Usuario user6(3, "?");
-
-	colaUsuarios->insert(user, user.getPriority());
-	colaUsuarios->insert(user2, user2.getPriority());
-	colaUsuarios->insert(user3, user3.getPriority());
-	colaUsuarios->insert(user4, user4.getPriority());
-	colaUsuarios->insert(user5, user5.getPriority());
-	colaUsuarios->insert(user6, user6.getPriority());
-	colaUsuarios->print();
-
-
-	Tiquete ticket(user.getPriority(), 1, "A", to_string(100));
-	Tiquete ticket2(user2.getPriority(), 2, "B", to_string(101));
-	Tiquete ticket3(user3.getPriority(), 1,  "C", to_string(102));
-	Tiquete ticket4(user4.getPriority(), 3, "A", to_string(103));
-	Tiquete ticket5(user5.getPriority(), 1,  "C", to_string(104));
-	Tiquete ticket6(user6.getPriority(), 4, "C", to_string(105));
-
-	colaTiquetes->insert(ticket, ticket.getFinalPriority());
-	colaTiquetes->insert(ticket2, ticket2.getFinalPriority());
-	colaTiquetes->insert(ticket3, ticket3.getFinalPriority());
-	colaTiquetes->insert(ticket4, ticket4.getFinalPriority());
-	colaTiquetes->insert(ticket5, ticket5.getFinalPriority());
-	colaTiquetes->insert(ticket6, ticket6.getFinalPriority());
-	colaTiquetes->print();
+	menuPrincipal(colaUsuarios);
 
 	delete colaUsuarios;
-	delete colaTiquetes;
-
 	return 0;
 }
