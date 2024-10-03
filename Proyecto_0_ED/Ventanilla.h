5359ca68da4d9d1ba437ed5b95f0fc2116b61057
BritanyRH2125 <61550366+BritanyRH2125@users.noreply.github.com>
2024-10-03 08:52:57 -0600

    Ventanilla.h

--- Proyecto_0_ED/Ventanilla.h
+++ Proyecto_0_ED/Ventanilla.h
@@ -10,64 +10,52 @@ using std::string;
 using std::time_t;
 using std::time;
 using std::tm;
+using std::runtime_error;
 
 class Ventanilla {
-public:
+private:
     string codigoVentanilla;     
     string tiqueteActual;         
     tm horaAtencion;              
 
-    Ventanilla() : tiqueteActual("N/A") {}
-    
-    Ventanilla(string areaCode, int numeroVentanilla) {
-        this->codigoVentanilla = areaCode + std::to_string(numeroVentanilla);
-        this->tiqueteActual = "N/A"; 
+public:
+    Ventanilla() : codigoVentanilla(""), tiqueteActual("N/A") {
+        // Inicializar horaAtencion a cero
+        std::memset(&horaAtencion, 0, sizeof(horaAtencion));
+    }
+
+    Ventanilla(const string& areaCode, int numeroVentanilla) 
+        : codigoVentanilla(areaCode + std::to_string(numeroVentanilla)), 
+          tiqueteActual("N/A") {
+        std::memset(&horaAtencion, 0, sizeof(horaAtencion));
     }
 
     // Método para que la ventanilla atienda un tiquete
     void atenderTiquete(PriorityQueue<Tiquete>& colaPrioridad) {
         if (colaPrioridad.isEmpty()) {
-            std::cout << "No hay tiquetes en la cola de prioridad.\n";
-            return;
+            throw runtime_error("No hay tiquetes en la cola de prioridad.");
         }
 
-        //  tiquete con mayor prioridad
+        // Tiquete con mayor prioridad
         Tiquete tiqueteAtendido = colaPrioridad.removeMin();
         this->tiqueteActual = tiqueteAtendido.getCode();
 
-        // hora de atención
+        // Hora de atención en UTC
         time_t tiempoActual = time(0);
-        tm utcTime;
-        gmtime_s(&utcTime, &tiempoActual);
+        horaAtencion = *gmtime(&tiempoActual); // Usar la hora en UTC
 
-        const int offset_seconds = -6 * 3600; //  zona horaria GMT-6
-        time_t gmt_minus_6 = tiempoActual + offset_seconds;
-
-        tm gmtMinus6Time;
-        gmtime_s(&gmtMinus6Time, &gmt_minus_6);
-
-        this->horaAtencion = gmtMinus6Time;
-
-        // tiquete atendido
+        // Tiquete atendido
         std::cout << "Tiquete atendido en la " << this->codigoVentanilla << ":\n";
         std::cout << tiqueteAtendido << std::endl;
-        std::cout << "Hora de atención: " << horaAtencion.tm_hour << ":"
-                  << (horaAtencion.tm_min < 10 ? "0" : "") << horaAtencion.tm_min << ":"
+        std::cout << "Hora de atención: " << horaAtencion.tm_hour << ":" 
+                  << (horaAtencion.tm_min < 10 ? "0" : "") << horaAtencion.tm_min << ":" 
                   << (horaAtencion.tm_sec < 10 ? "0" : "") << horaAtencion.tm_sec << "\n";
     }
 
-    
-    void operator =(const Ventanilla& other) {
-        this->codigoVentanilla = other.codigoVentanilla;
-        this->tiqueteActual = other.tiqueteActual;
-        this->horaAtencion = other.horaAtencion;
-    }
-
-    //imprimir la ventanilla
-    friend ostream& operator <<(ostream& os, const Ventanilla& ventanillaInfo) {
+    // Imprimir la ventanilla
+    friend ostream& operator<<(ostream& os, const Ventanilla& ventanillaInfo) {
         os << "Ventanilla: " << ventanillaInfo.codigoVentanilla 
            << " | Tiquete Actual: " << ventanillaInfo.tiqueteActual;
         return os;
     }
 };
-
